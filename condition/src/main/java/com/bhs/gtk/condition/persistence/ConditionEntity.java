000655a3c953f722d222a00c39b36f937227c707
Bhuvaneshwar Bharathiya K R <Bhuvaneshwar Bharathiya K R  Bhuvaneshwar.Bharathiya@gmail.com>
2021-09-14 17:01:14 +0530

    Add persistence package
    add entity and repository classes

---
+++ condition/src/main/java/com/bhs/gtk/condition/persistence/ConditionEntity.java
@@ -0,0 +1,71 @@
+package com.bhs.gtk.condition.persistence;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+public class ConditionEntity {
+	
+
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	private UUID id;
+	
+	@Column(length = PersistenceConstants.SMALL_TEXT_LIMIT)
+	private String name;
+	@Column(length = PersistenceConstants.LARGE_TEXT_LIMIT)
+	private String description;
+	@Column(length = PersistenceConstants.LARGE_TEXT_LIMIT)
+	private String code;
+	@Column(length = PersistenceConstants.LARGE_TEXT_LIMIT)
+	private String parseTree;
+	
+	protected ConditionEntity() {}
+	
+	public ConditionEntity(String name, String description, String code, String parseTree) {
+		this.name = name;
+		this.description = description;
+		this.code = code;
+		this.parseTree = parseTree;
+	}
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getParseTree() {
+		return parseTree;
+	}
+	public void setParseTree(String parseTree) {
+		this.parseTree = parseTree;
+	}
+
+}
