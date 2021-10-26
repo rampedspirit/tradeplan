8840d21418670941c91252664404839eddcb5d0b
Sameer Nage Gowda C V <sameer.tumkur@gmail.com>
2021-10-26 23:30:24 +0530

    updated LB url

--- frontend/greentick-webapp/src/environments/environment.prod.ts
+++ frontend/greentick-webapp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  filterApiBasePath: "http://AppServiceLoadBalancer-1350731698.ap-south-1.elb.amazonaws.com",
-  conditionApiBasePath: "http://AppServiceLoadBalancer-1350731698.ap-south-1.elb.amazonaws.com",
-  screenerApiBasePath: "http://AppServiceLoadBalancer-1350731698.ap-south-1.elb.amazonaws.com"
+  filterApiBasePath: "http://AppServiceLoadBalancer-418442467.ap-south-1.elb.amazonaws.com",
+  conditionApiBasePath: "http://AppServiceLoadBalancer-418442467.ap-south-1.elb.amazonaws.com",
+  screenerApiBasePath: "http://AppServiceLoadBalancer-418442467.ap-south-1.elb.amazonaws.com"
 };
