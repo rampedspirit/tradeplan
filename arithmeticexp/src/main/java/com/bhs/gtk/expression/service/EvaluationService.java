ba78e015d8a3054513e3dc70e9ea9b2b721f633a
Bhuvaneshwar Bharathiya K R <Bhuvaneshwar Bharathiya K R  Bhuvaneshwar.Bharathiya@gmail.com>
2021-11-11 13:48:28 +0530

    [Refactor] move conversion of evaluation request to expression model to separate class.

--- arithmeticexp/src/main/java/com/bhs/gtk/expression/service/EvaluationService.java
+++ arithmeticexp/src/main/java/com/bhs/gtk/expression/service/EvaluationService.java
@@ -1,9 +1,7 @@
 package com.bhs.gtk.expression.service;
 
-import com.bhs.gtk.expression.model.Expression;
 import com.bhs.gtk.expression.model.EvaluationResponse;
 
 public interface EvaluationService {
-	public Expression convert(String evalRequest);
 	public EvaluationResponse evaluate(String evalRequest);
 }
