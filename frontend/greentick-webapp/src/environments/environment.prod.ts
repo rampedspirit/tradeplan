ff6120081335ef4991c662080630f9ef777481e6
Sameer Nage Gowda C V <sameer.tumkur@gmail.com>
2021-11-25 15:35:34 +0530

    updated app service load balancer url

--- frontend/greentick-webapp/src/environments/environment.prod.ts
+++ frontend/greentick-webapp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  filterApiBasePath: "http://AppServiceLoadBalancer-418442467.ap-south-1.elb.amazonaws.com",
-  conditionApiBasePath: "http://AppServiceLoadBalancer-418442467.ap-south-1.elb.amazonaws.com",
-  screenerApiBasePath: "http://AppServiceLoadBalancer-418442467.ap-south-1.elb.amazonaws.com"
+  filterApiBasePath: "http://AppServiceLoadBalancer-1150912334.ap-south-1.elb.amazonaws.com",
+  conditionApiBasePath: "http://AppServiceLoadBalancer-1150912334.ap-south-1.elb.amazonaws.com",
+  screenerApiBasePath: "http://AppServiceLoadBalancer-1150912334.ap-south-1.elb.amazonaws.com"
 };
