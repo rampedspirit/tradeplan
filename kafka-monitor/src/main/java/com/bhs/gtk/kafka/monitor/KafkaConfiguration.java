b0d59670a9ad7c80ee909e85f8e1a714bc5725f6
Sameer Nage Gowda C V <sameer.tumkur@gmail.com>
2022-01-09 10:35:34 +0530

    temporarily disabled kafka admin monitor

--- kafka-monitor/src/main/java/com/bhs/gtk/kafka/monitor/KafkaConfiguration.java
+++ kafka-monitor/src/main/java/com/bhs/gtk/kafka/monitor/KafkaConfiguration.java
@@ -6,21 +6,19 @@ import java.util.Map;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
 
-@Configuration
+//@Configuration
 public class KafkaConfiguration {
-    
-    @Value(value = "${kafka.bootstrapAddress}")
-    private String bootstrapAddress;
 
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        return new KafkaAdmin(configs);
-    }
-    
-    
+	@Value(value = "${kafka.bootstrapAddress}")
+	private String bootstrapAddress;
+
+	@Bean
+	public KafkaAdmin kafkaAdmin() {
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		return new KafkaAdmin(configs);
+	}
+
 }
--- kafka-monitor/src/main/java/com/bhs/gtk/kafka/monitor/KafkaProducerConfiguration.java
+++ kafka-monitor/src/main/java/com/bhs/gtk/kafka/monitor/KafkaProducerConfiguration.java
@@ -7,17 +7,16 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-@Configuration
+//@Configuration
 public class KafkaProducerConfiguration {
-	
-   @Value(value = "${kafka.bootstrapAddress}")
-   private String bootstrapAddress;
-	
+
+	@Value(value = "${kafka.bootstrapAddress}")
+	private String bootstrapAddress;
+
 	@Bean
 	public ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> configProperties = new HashMap<>();
@@ -26,7 +25,7 @@ public class KafkaProducerConfiguration {
 		configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return new DefaultKafkaProducerFactory<>(configProperties);
 	}
-	
+
 	@Bean
 	public KafkaTemplate<String, String> kafkaTemplate() {
 		return new KafkaTemplate<>(producerFactory());
