9415f3c353ae11bc449c5e862a7dec3b08f23c1b
Sameer Nage Gowda C V <sameer.tumkur@gmail.com>
2022-02-16 17:16:44 +0530

    updated filter api controller to create filter without code and parsetree

--- filter/src/main/java/com/bhs/gtk/filter/persistence/FilterEntity.java
+++ filter/src/main/java/com/bhs/gtk/filter/persistence/FilterEntity.java
@@ -20,7 +20,7 @@ public class FilterEntity {
 	@GeneratedValue(generator = "UUID")
 	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
 	private UUID id;
-	
+
 	@Column(length = PersistenceConstants.SMALL_TEXT_LIMIT)
 	private String name;
 	@Column(length = PersistenceConstants.LARGE_TEXT_LIMIT)
@@ -29,46 +29,54 @@ public class FilterEntity {
 	private String code;
 	@Column(length = PersistenceConstants.LARGE_TEXT_LIMIT)
 	private String parseTree;
-	
+
 	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private List<ExpressionEntity> expressions;
-	
-	protected FilterEntity() {}
-	
-	public FilterEntity(String name, String description, String code, String parseTree) {
+
+	protected FilterEntity() {
+	}
+
+	public FilterEntity(String name, String description) {
 		this.name = name;
 		this.description = description;
-		this.code = code;
-		this.parseTree = parseTree;
 	}
-	
+
 	public UUID getId() {
 		return id;
 	}
+
 	public void setId(UUID id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getCode() {
 		return code;
 	}
+
 	public void setCode(String code) {
 		this.code = code;
 	}
+
 	public String getParseTree() {
 		return parseTree;
 	}
+
 	public void setParseTree(String parseTree) {
 		this.parseTree = parseTree;
 	}
