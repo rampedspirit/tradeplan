8ad94902883a6a38b17915cba575e57bb8219b90
Bhuvan <Bhuvaneshwar.bharathiya@gmail.com>
2022-02-20 20:00:57 +0530

    ES: remove old classes in messaging package
    removing this to add new classes as per message patterns defined in
    other services

--- arithmeticexp/src/main/java/com/bhs/gtk/expression/messaging/MessageConstants.java
+++
@@ -1,7 +0,0 @@
-package com.bhs.gtk.expression.messaging;
-
-public class MessageConstants {
-	public static final String INPUT_TOPIC_NAME = "execution_service_input";
-	public static final String OUTPUT_TOPIC_NAME = "execution_service_output";
-
-}
