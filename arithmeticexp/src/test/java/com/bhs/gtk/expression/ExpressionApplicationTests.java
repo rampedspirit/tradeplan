5b4c03b117dadabaee65c9fa6bcadb88928dd66f
Sameer Nage Gowda C V <sameer.tumkur@gmail.com>
2022-03-01 17:26:07 +0530

    removed test case for expression service

--- arithmeticexp/src/test/java/com/bhs/gtk/expression/ExpressionApplicationTests.java
+++ arithmeticexp/src/test/java/com/bhs/gtk/expression/ExpressionApplicationTests.java
@@ -1,13 +1,8 @@
 package com.bhs.gtk.expression;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ExpressionApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
