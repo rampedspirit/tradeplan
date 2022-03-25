df4d8d7b5c6dd7df6e354233c7a830871d1f48e1
Bhuvan <Bhuvaneshwar.bharathiya@gmail.com>
2022-03-25 12:22:16 +0530

    CS: refactor : remove result variable in condition expression
    The variable is not required. The result derived is consumed
    immediately. Also condition expression as a model is not saved, hence,
    the variable is redundant.

--- condition/src/main/java/com/bhs/gtk/condition/model/ConditionExpression.java
+++ condition/src/main/java/com/bhs/gtk/condition/model/ConditionExpression.java
@@ -1,14 +1,4 @@
 package com.bhs.gtk.condition.model;
 
 public abstract class ConditionExpression {
-
-	private String result; //PASS/FAIL/ERROR
-	
-	public String getResult() {
-		return result;
-	}
-
-	public void setResult(String result) {
-		this.result = result;
-	}
 }
